37ee023e12e148d8a15b0dbdbbfb0fc832cde77f
Asher <godwindiya441@gmail.com>
2026-07-03 23:02:52 +0100

    Add blood glucose checker logic

--- app/health-check/page.tsx
+++ app/health-check/page.tsx
@@ -6,6 +6,20 @@ export default function HealthCheckPage() {
     const [glucose, setGlucose] = useState("");
     const [result, setResult] = useState("");
 
+    function checkResult() {
+        const glucoseNumber = Number(glucose);
+        if (glucoseNumber < 70) {
+            setResult("🟠 Low Blood Glucose");
+        } else if (glucoseNumber <= 99) {
+            setResult("🟢 Normal Blood Glucose");
+        } else {
+            setResult("🔴 High Blood Glucose");
+        }
+    }
+
+    <p className="mt-6 text-center text-xl font-semibold text-green-700">
+        {result}
+    </p>
 
     return (
         <main className="min-h-screen flex flex-col items-center justify-center bg-slate-50">
@@ -31,6 +45,33 @@ export default function HealthCheckPage() {
             <p className="mt-4 text-blue-700">
                 Your age is: {age}
             </p>
+            </div>
+
+            <div className="mt-6">
+                <label className="block font-medium">
+                    Blood Glucose (mg/dL) 
+                </label>
+
+                <input 
+                        type="number"
+                        placeholder="Enter your blood glucose"
+                        value={glucose}
+                        onChange={(event) => 
+                            setGlucose(event.target.value)}
+                        className="w-full border rounded-lg p-3 mt-2"
+                    />
+                    <button
+                        onClick={checkResult}
+                        className="w-full bg-blue-700 text-white py-3 rounded-lg mt-6 hover:bg-blue-800">
+                        Check Result
+                    </button>
+
+                    <p className="mt-6 text-center text-xl font-semibold text-green-700">
+                        {result}
+                    </p>
+                    
+
+
             </div>
 
         </div>
